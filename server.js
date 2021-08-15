a094e278ca1f11fe5f3436444166682386e43ba5
Dave Spina <dspina79@gmail.com>
2021-08-14 21:21:51 -0400

    Added graphQL dependencies:

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql').graphqlHTTP;
 const app = express();
 const port = process.env.PORT || 3000;
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString
+} = require('graphql');
+
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'FirstSchema',
+        fields:  () => ({
+            message: {type: GraphQLString, 
+                resolve: () => 'Hello World'
+            },
+        })
+    })
+});
+
+app.use('/graphql', expressGraphQL({
+    schema: schema,
+    graphiql: true
+}));
 
 app.listen(port, () => {
     console.log(`Running server on ${port}`);
